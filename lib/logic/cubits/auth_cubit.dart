8ae816e2ccc2c9579a33fc34fcd4ec0dfab53988
Yassine <hennimohammedyassine@gmail.com>
2026-01-10 21:52:26 +0100

    remove multiuser

--- lib/logic/cubits/auth_cubit.dart
+++ lib/logic/cubits/auth_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../data/repositories/auth_repository.dart';
-import '../../data/services/user_switcher_service.dart';
 import 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
@@ -82,22 +81,5 @@ class AuthCubit extends Cubit<AuthState> {
     await _authRepository.logout();
     
     emit(AuthUnauthenticated());
-  }
-
-  // Switch to another user
-  // - Updates SharedPreferences with new user ID
-  // - Clears all tokens (logout)
-  // - Emits AuthUnauthenticated state
-  Future<void> switchUser(String userId) async {
-    emit(AuthLoading());
-    
-    final success = await UserSwitcherService.switchUser(userId);
-    
-    if (success) {
-      emit(AuthUnauthenticated());
-    } else {
-      emit(const AuthError('Failed to switch user'));
-      emit(AuthUnauthenticated());
-    }
   }
 }
